c8d3f3bb9d126d075a04c63f884b135e287e5f99
TakahashiTaiki <oibc35150@ojs.ac.jp>
2024-04-24 15:57:14 +0900

    問題2.2完成

--- Chapter02/演習問題/Exersize2/Program.cs
+++ Chapter02/演習問題/Exersize2/Program.cs
@@ -7,7 +7,12 @@ using System.Threading.Tasks;
 namespace Exersize2 {
     internal class Program {
         static void Main(string[] args) {
-            
+            double ratio = 0.0254;//定数
+            for (int inti = 1; inti <= 10; inti++) {
+                double meter = inti * ratio;
+                Console.WriteLine("{0}インチ = {1:0.0000}m", inti, meter);
+            }
         }
+        
     }
 }
