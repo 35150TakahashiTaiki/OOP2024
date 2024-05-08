a6456a078e7446d9db8c8a1d876a641261d31f44
TakahashiTaiki <oibc35150@ojs.ac.jp>
2024-05-08 13:40:25 +0900

    マウスクリック座標から開始

--- FormApps/BallApp/Form1.cs
+++ FormApps/BallApp/Form1.cs
@@ -1,43 +1,38 @@
 namespace BallApp {
     public partial class Form1 : Form {
+        SoccerBall soccerBall;
+        PictureBox pb;
+
         private double posX;//x座標
         private double posY;//y座標
         private double moveX;//移動量(x方向)
         private double moveY;//移動量(y方向)
-        
+
 
         //コンストラクタ
         public Form1() {
             InitializeComponent();
-
-            moveX = moveY = 5;
         }
         //フォームが最初にロードされるとき一度だけ実行される
         private void Form1_Load(object sender, EventArgs e) {
-            //this.BackColor = Color.Aquamarine;
-            timer1.Start();
+           
         }
 
         private void timer1_Tick(object sender, EventArgs e) {
-            this.Text = pbBall.Location.ToString();//現在地を表示
-           
-            if(pbBall.Location.X >= 750 || pbBall.Location.X< 0) {
-                moveX = -moveX;
-            }
-            if (pbBall.Location.Y >= 500 || pbBall.Location.Y < 0) {
-                moveY = -moveY;
-            }
-           
-                posX += moveX;
-           
-            
-                posY += moveY;
-         
-            
-            
-            pbBall.Location = new Point((int)posX, (int)posY);
+            soccerBall.Move();
+            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
 
-            
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e) {
+            pb = new PictureBox();//画像を表示する
+            pb.Size = new Size(50, 50);
+            soccerBall = new SoccerBall(e.X, e.Y);
+            pb.Image = soccerBall.Image;
+            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb.Parent = this;
+            timer1.Start();
         }
     }
 }
