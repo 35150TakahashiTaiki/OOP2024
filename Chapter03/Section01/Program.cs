47fc193e7efc4c14d3e85626d0ed815c221d3b63
TakahashiTaiki <oibc35150@ojs.ac.jp>
2024-05-15 11:48:42 +0900

    p87まで

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            //var count = numbers.Count(n => n%2== 0);
+            //var count = Count(numbers,n => n >= 5 && n<10);
+            //var count = Count(numbers, n => n.ToString().Contains('1'));
+            double num = numbers.Where(n => n >= 5).Average();
+            int total = numbers.Where(n => n >= 5).Sum();
+            Console.WriteLine(num);
+            Console.WriteLine(total);
+        }
+
+        
+
+
+        /*public static int Count(int[] numbers, Predicate<int> judge) {
+            int count = 0;
+            foreach (var n in numbers) {
+                if(judge(n) == true)
+                    count++;
+            }
+            return count;
+       
+        } */
+
+    }
+}
