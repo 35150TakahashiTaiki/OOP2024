ac04cf38231fa2b36c389cd27b0bc3d57af5ba0f
TakahashiTaiki <oibc35150@ojs.ac.jp>
2024-05-15 15:58:00 +0900

    演習問題3.2作成

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -12,11 +12,18 @@ namespace Section01 {
                 "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
             };
 
-            IEnumerable<string> query = names.Where(s => s.Contains(' '))
-                                             .Select(s => s.ToUpper());
-            foreach (string s in query) { 
-            Console.WriteLine(s);
+            var query = names.Where(s => s.Length <= 5).ToList();
+            foreach (var item in query) {
+                Console.WriteLine(item);
             }
+
+            Console.WriteLine("---------------");
+
+            names[0] = "Osaka";
+            foreach (var item in query) {
+                Console.WriteLine(item);
+            }
+            
         }
     }
 }
