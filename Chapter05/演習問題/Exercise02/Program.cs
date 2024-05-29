a4557c1df7f0fa605de2b867aaf883bc3dd195c5
TakahashiTaiki <oibc35150@ojs.ac.jp>
2024-05-29 15:45:29 +0900

    5.1完成

---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            int number;
+            Console.Write("数字入力：");
+            string str = Console.ReadLine();        
+            bool result = int.TryParse(str,out number);
+            if (result) {
+                Console.WriteLine($"{str,8:#,0}");
+            } else {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
---
+++ Chapter05/演習問題/Exercize01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("文字入力１：");
+            var str1 = Console.ReadLine();
+            Console.Write("文字入力２：");
+            var str2 = Console.ReadLine();
+
+            if(String.Compare(str1,str2,true) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+}
