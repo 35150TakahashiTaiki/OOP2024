42a4a10731674d3d994c1dd77bdefdd4593b6c28
TakahashiTaiki <oibc35150@ojs.ac.jp>
2024-06-11 15:52:44 +0900

    一期目　7章

--- Chapter05/演習問題/Exercise04/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Exercise04/Program.cs
@@ -3,29 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Exercise04 {
     internal class Program {
         static void Main(string[] args) {
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-            string[] subs = line.Split(';');
-            foreach (var sub in subs) {
-                string[] cut = sub.Split('=');
-                foreach (var cut2 in cut) {
-                    if (cut2.Contains("Novelist")) {
-                        var chang = cut2.Replace("Novelist", "作家　：");
-                    }
-                    Console.WriteLine(cut2);
-                }
+            foreach (var pair in line.Split(';')) {
+                var array = pair.Split('=');
+                Console.WriteLine("{0}：{1}", ToJapanese(array[0]), array[1]);
             }
-
         }
 
         //できたら以下のメソッドを完成させて利用する
-        //static string ToJapanese(string key) {
+        static string ToJapanese(string key) {
 
+            switch (key) {
+                case "Novelist":
+                    return "作家　";
 
-        //}
+                case "BestWork":
+                    return "代表作";
+
+                case "Born":
+                    return "誕生年";
+            }
+            throw new ArgumentException("引数に誤りがあります");
+        }
     }
 }
