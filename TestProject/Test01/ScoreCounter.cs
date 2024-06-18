9193940b0ffb515737087b48de1b769ae87085b7
TakahashiTaiki <oibc35150@ojs.ac.jp>
2024-06-18 11:10:58 +0900

    設問１_提出

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -13,7 +13,18 @@ namespace Test01 {
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-
+             List<Student> sales = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Student sale = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
 
 
 
@@ -22,7 +33,15 @@ namespace Test01 {
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
-
+            var dict = new Dictionary<string, int>();
+            foreach (var sale in _score) {
+                if (dict.ContainsKey(sale.Name)) {
+                    dict[sale.Name] += sale.Score;
+                } else {
+                    dict[sale.Name] = sale.Score;
+                }
+            }
+            return dict;
 
 
 
