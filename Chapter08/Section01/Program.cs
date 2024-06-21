61f7aa306ee1aa3019aac31a1388a71b9c8105b7
TakahashiTaiki <oibc35150@ojs.ac.jp>
2024-06-21 11:24:01 +0900

    和暦＆生まれてからの日数

--- Chapter08/Section01/Program.cs
+++ Chapter08/Section01/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,13 +18,27 @@ namespace Section01 {
             int day = int.Parse(Console.ReadLine());
 
             var birthday = new DateTime(year, month, day);
+
+            #region 誕生日確認
             DayOfWeek dayOfWeek = birthday.DayOfWeek;
             string japaneseDayOfWeek = GetJapaneseDayOfWeek(dayOfWeek);
-
             Console.WriteLine("あなたは{0}に生まれました", japaneseDayOfWeek);
+            #endregion
 
+            #region 和暦表示
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var str = birthday.ToString("ggyy年M月d日", culture);
+            Console.WriteLine("あなたは{0}に生まれました",str);
+            #endregion
 
+            #region 生まれてからの日数
+            var now =DateTime.Now;
+            TimeSpan ts1 = now - birthday;
+            Console.WriteLine("あなたは生まれてから今日で{0}日目です",ts1.Days+1);
+            #endregion
 
+            #region 閏年判定
             /*var dt1 = new DateTime(2024,6,19);
             var dt2 = new DateTime(2012,4 , 15, 8, 45, 20);
             Console.WriteLine(dt1);
@@ -40,8 +55,10 @@ namespace Section01 {
             }else {
                 Console.WriteLine("閏年ではない");
             }*/
+            #endregion
         }
 
+        //曜日を日本語に変換
         public static string GetJapaneseDayOfWeek(DayOfWeek dayOfWeek) {
             switch (dayOfWeek) {
                 case DayOfWeek.Sunday:
