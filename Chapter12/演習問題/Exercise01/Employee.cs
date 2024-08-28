987a03bd8129ccd15e5267400080eedb20f846ff
TakahashiTaiki <oibc35150@ojs.ac.jp>
2024-08-28 15:54:14 +0900

    12.4 キー名のカスタマイズ

--- Chapter12/演習問題/Exercise01/Employee.cs
+++ Chapter12/演習問題/Exercise01/Employee.cs
@@ -5,9 +5,9 @@ using System.Text.Json.Serialization;
 public class Employee {
     [JsonIgnore]
     public int Id { get; set; }
-    [JsonPropertyName("name")]
+    
     public string Name { get; set; }
-    [JsonPropertyName("hireDate")]
+    
     public DateTime HireDate { get; set; }
     public override string ToString() {
         return string.Format("[Id={0}, Name={1}, HireDate={2}]",
