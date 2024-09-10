b4444267e9362a069ceeb0740fd2efbff8190712
TakahashiTaiki <oibc35150@ojs.ac.jp>
2024-09-10 12:06:03 +0900

    2024/9/10 webView完成

--- FormApps/RssReader/Form1.Designer.cs
+++ FormApps/RssReader/Form1.Designer.cs
@@ -23,22 +23,20 @@
         /// コード エディターで変更しないでください。
         /// </summary>
         private void InitializeComponent() {
-            this.tbRssUrl = new System.Windows.Forms.TextBox();
             this.btGet = new System.Windows.Forms.Button();
             this.lbRssTitle = new System.Windows.Forms.ListBox();
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btToroku = new System.Windows.Forms.Button();
+            this.cbRssUrl = new System.Windows.Forms.ComboBox();
+            this.cbName = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
             this.SuspendLayout();
-            // 
-            // tbRssUrl
-            // 
-            this.tbRssUrl.Location = new System.Drawing.Point(33, 22);
-            this.tbRssUrl.Name = "tbRssUrl";
-            this.tbRssUrl.Size = new System.Drawing.Size(586, 19);
-            this.tbRssUrl.TabIndex = 0;
             // 
             // btGet
             // 
-            this.btGet.Location = new System.Drawing.Point(646, 18);
+            this.btGet.Location = new System.Drawing.Point(761, 16);
             this.btGet.Name = "btGet";
             this.btGet.Size = new System.Drawing.Size(75, 23);
             this.btGet.TabIndex = 1;
@@ -50,43 +48,97 @@
             // 
             this.lbRssTitle.FormattingEnabled = true;
             this.lbRssTitle.ItemHeight = 12;
-            this.lbRssTitle.Location = new System.Drawing.Point(33, 83);
+            this.lbRssTitle.Location = new System.Drawing.Point(33, 143);
             this.lbRssTitle.Name = "lbRssTitle";
-            this.lbRssTitle.Size = new System.Drawing.Size(586, 232);
+            this.lbRssTitle.Size = new System.Drawing.Size(266, 520);
             this.lbRssTitle.TabIndex = 2;
             this.lbRssTitle.Click += new System.EventHandler(this.lbRssTitle_Click);
             // 
-            // webBrowser1
+            // webView21
             // 
-            this.webBrowser1.Location = new System.Drawing.Point(33, 321);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(904, 338);
-            this.webBrowser1.TabIndex = 3;
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(305, 143);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(777, 516);
+            this.webView21.TabIndex = 4;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "URL又はお気に入り名称：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "お気に入り名称：";
+            // 
+            // btToroku
+            // 
+            this.btToroku.Location = new System.Drawing.Point(761, 63);
+            this.btToroku.Name = "btToroku";
+            this.btToroku.Size = new System.Drawing.Size(75, 23);
+            this.btToroku.TabIndex = 8;
+            this.btToroku.Text = "登録";
+            this.btToroku.UseVisualStyleBackColor = true;
+            // 
+            // cbRssUrl
+            // 
+            this.cbRssUrl.FormattingEnabled = true;
+            this.cbRssUrl.Location = new System.Drawing.Point(160, 18);
+            this.cbRssUrl.Name = "cbRssUrl";
+            this.cbRssUrl.Size = new System.Drawing.Size(586, 20);
+            this.cbRssUrl.TabIndex = 9;
+            // 
+            // cbName
+            // 
+            this.cbName.FormattingEnabled = true;
+            this.cbName.Location = new System.Drawing.Point(160, 55);
+            this.cbName.Name = "cbName";
+            this.cbName.Size = new System.Drawing.Size(586, 20);
+            this.cbName.TabIndex = 10;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(995, 671);
-            this.Controls.Add(this.webBrowser1);
+            this.ClientSize = new System.Drawing.Size(1142, 751);
+            this.Controls.Add(this.cbName);
+            this.Controls.Add(this.cbRssUrl);
+            this.Controls.Add(this.btToroku);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webView21);
             this.Controls.Add(this.lbRssTitle);
             this.Controls.Add(this.btGet);
-            this.Controls.Add(this.tbRssUrl);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox tbRssUrl;
         private System.Windows.Forms.Button btGet;
         private System.Windows.Forms.ListBox lbRssTitle;
-        private System.Windows.Forms.WebBrowser webBrowser1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btToroku;
+        private System.Windows.Forms.ComboBox cbRssUrl;
+        private System.Windows.Forms.ComboBox cbName;
     }
 }
 
--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -12,34 +12,81 @@ using System.Windows.Forms;
 using System.Xml;
 using System.Xml.Linq;
 using static System.Windows.Forms.LinkLabel;
+using Microsoft.Web.WebView2.Core;
+using Windows.UI.Xaml.Controls;
+using System.Threading;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace RssReader {
     public partial class Form1 : Form {
+        readonly CountdownEvent condition = new CountdownEvent(1);
         public Form1() {
+
             InitializeComponent();
+            
         }
+
+        async void InitializeAsync() {
+            await webView21.EnsureCoreWebView2Async(null);
+            webView21.CoreWebView2.NavigationCompleted += webView21_NavigationCompleted;
+        }
+
+        List<ItemData> xitems;
 
         private void btGet_Click(object sender, EventArgs e) {
             using(var wc = new WebClient()) {
-                var url = wc.OpenRead(tbRssUrl.Text);
+                var url = wc.OpenRead(cbRssUrl.Text);
                 var xdoc = XDocument.Load(url);
 
-                foreach (var item in xdoc.Root.Descendants("item"))
+                 xitems = xdoc.Root.Descendants("item").Select(item => new ItemData {
+                    Title = item.Element("title").Value,
+                    Link = item.Element("link").Value,
+
+                }).ToList();
+
+                foreach (var item in xitems)
                 {
-                    XElement xtitle = item.Element("title");
-                    XElement xlink = item.Element("link");
-                    lbRssTitle.Items.Add(xtitle.Value);
+                   
+                    lbRssTitle.Items.Add(item.Title);
+                    
                 }
 
             }
         }
 
         private void Form1_Load(object sender, EventArgs e) {
-            webBrowser1.ScriptErrorsSuppressed = true;
+            InitializeAsync();
+            List<string> topics = new List<string> {
+                "主要", "国内", "国際", "経済", "エンタメ", "スポーツ", "IT", "科学", "地域"
+            };
+
+
+            cbRssUrl.Items.AddRange(topics.ToArray());
         }
 
         private void lbRssTitle_Click(object sender, EventArgs e) {
-            webBrowser1.Navigate("xlink.Value");
+            
+            
+            webView21.CoreWebView2.Navigate(xitems[lbRssTitle.SelectedIndex].Link);
         }
+        
+        public class ItemData {
+            public string Title { get; set; }
+            public string Link { get; set; }
+        }
+
+        private void webView21_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e) {
+            //読み込み結果を判定
+            if (e.IsSuccess)
+                Console.WriteLine("complete");
+            else
+                Console.WriteLine(e.WebErrorStatus);
+
+            //シグナル初期化
+            condition.Signal();
+            System.Threading.Thread.Sleep(1);
+            condition.Reset();
+        }
+
     }
 }
