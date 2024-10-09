13f07abecdf07997ecb23d7e84046954d51ad93b
NegishiYu <oibc35141@ojs.ac.jp>
2024-10-09 14:56:54 +0900

    LineCounter

---
+++ Chapter17/TemplateMethod/LineCounter/LineCounterProcesser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFileProcessor;
+using System.IO;
+
+namespace LineCounter {
+     class LineCounterProcesser : TextProcessor{
+
+        private int _count;
+        string _text = "";
+
+        protected override void Initialize(string fname) {
+            _count = 0;
+
+        }
+
+        protected override void Execute(string line) {
+            Console.WriteLine(line.ToLower());
+            _text = line;
+            _count++;
+        }
+
+        protected override void Terminate() {
+            
+            Console.WriteLine("{0}行",_count);
+        }
+    }
+}
